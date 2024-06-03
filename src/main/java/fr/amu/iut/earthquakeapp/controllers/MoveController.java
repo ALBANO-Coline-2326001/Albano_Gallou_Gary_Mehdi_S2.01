37567fdaf2653a75dbf6cee6d073f7cff19d938c
yousra <yousramehdi38@gmail.com>
2024-06-03 12:09:15 +0200

    Debut du Controlleur de mouvement

--- src/main/java/fr/amu/iut/earthquakeapp/controllers/MoveController.java
+++ src/main/java/fr/amu/iut/earthquakeapp/controllers/MoveController.java
@@ -1,4 +1,15 @@
 package fr.amu.iut.earthquakeapp.controllers;
 
+import fr.amu.iut.earthquakeapp.jeu.Board;
+
 public class MoveController {
+
+    private Board chessBoard;
+    private int x;
+    private int y;
+
+    public void movePiece(){
+
+    }
+
 }
