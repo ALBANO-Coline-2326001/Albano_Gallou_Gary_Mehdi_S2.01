101aa52cbf647ffec7badb9aea6a9424f72e50ec
yousra <yousramehdi38@gmail.com>
2024-06-03 14:46:00 +0200

    Ajout de la classe abstract Pièce

--- src/main/java/fr/amu/iut/earthquakeapp/jeu/Board.java
+++ src/main/java/fr/amu/iut/earthquakeapp/jeu/Board.java
@@ -1,44 +1,39 @@
 package fr.amu.iut.earthquakeapp.jeu;
 
 import fr.amu.iut.earthquakeapp.jeu.pieces.*;
+import java.util.ArrayList;
 
 public class Board {
-        private Piece[][] board;
+    private ArrayList<ArrayList<Piece>> board;
 
     public Board() {
-        board = new Piece[8][8];
-        // initialiser le plateau avec les pièces
-        // 0 = case vide
-        // 1 = pion
-        // 2 = cavalier
-        // 3 = fou
-        // 4 = tour
-        // 5 = reine
-        // 6 = roi
+        board = new ArrayList<>();
+
         for (int i = 0; i < 8; i++) {
+            ArrayList<Piece> row = new ArrayList<>();
             for (int j = 0; j < 8; j++) {
-                boolean isWhite = i < 2; // les pièces des deux premières rangées sont blanches, les autres sont noires
+                boolean isWhite = i < 2;
                 if (i == 0 || i == 7) {
                     if (j == 0 || j == 7) {
-                        board[i][j] = new Tour(isWhite, i, j); // Tour
+                        row.add(new Tour(isWhite, i, j));
                     } else if (j == 1 || j == 6) {
-                        board[i][j] = new Cavalier(isWhite, i, j); // Cavalier
+                        row.add(new Cavalier(isWhite, i, j));
                     } else if (j == 2 || j == 5) {
-                        board[i][j] = new Fou(isWhite, i, j); // Fou
+                        row.add(new Fou(isWhite, i, j));
                     } else if (j == 3) {
-                        board[i][j] = new Reine(isWhite, i, j); // Reine
+                        row.add(new Reine(isWhite, i, j));
                     } else {
-                        board[i][j] = new Roi(isWhite, i, j); // Roi
+                        row.add(new Roi(isWhite, i, j));
                     }
                 } else if (i == 1 || i == 6) {
-                    board[i][j] = new Pion(isWhite, i, j); // Pion
+                    row.add(new Pion(isWhite, i, j));
                 } else {
-                    board[i][j] = null; // case vide
+                    row.add(null);
                 }
             }
+            board.add(row);
         }
     }
 
     // autres méthodes...
 }
-
