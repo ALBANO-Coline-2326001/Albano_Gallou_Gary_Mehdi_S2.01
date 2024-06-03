18db415c71bb85b4c375cab3d80b85d615a25d1e
yousra <yousramehdi38@gmail.com>
2024-06-03 11:50:31 +0200

    Ajout des pièces

--- src/main/java/fr/amu/iut/earthquakeapp/jeu/Cavalier.java
+++ src/main/java/fr/amu/iut/earthquakeapp/jeu/Cavalier.java
@@ -1,12 +1,10 @@
 package fr.amu.iut.earthquakeapp.jeu;
 
-public class Tour extends Piece{
+public class Cavalier extends Piece{
 
-
-    public Tour(boolean isWhite, int x, int y) {
+    public Cavalier(boolean isWhite, int x, int y) {
         super(isWhite, x, y);
     }
-
     @Override
     public void move(int x, int y) {
           this.setCoordonne(x,y);
--- src/main/java/fr/amu/iut/earthquakeapp/jeu/Fou.java
+++ src/main/java/fr/amu/iut/earthquakeapp/jeu/Fou.java
@@ -1,14 +1,12 @@
 package fr.amu.iut.earthquakeapp.jeu;
 
-public class Tour extends Piece{
+public class Fou extends Piece{
 
-
-    public Tour(boolean isWhite, int x, int y) {
+    public Fou(boolean isWhite, int x, int y) {
         super(isWhite, x, y);
     }
-
     @Override
     public void move(int x, int y) {
-          this.setCoordonne(x,y);
+       this.setCoordonne(x,y);
     }
 }
