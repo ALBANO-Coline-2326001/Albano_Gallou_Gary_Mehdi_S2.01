689f2f9ab6b7281aa79a1cd10ffb960d006bac9b
loicgll <loic.gallou@etu.univ-amu.fr>
2024-06-05 08:49:59 +0200

    fonctionnement roi fin

--- src/main/java/fr/amu/iut/earthquakeapp/jeu/pieces/Roi.java
+++ src/main/java/fr/amu/iut/earthquakeapp/jeu/pieces/Roi.java
@@ -1,6 +1,5 @@
 package fr.amu.iut.earthquakeapp.jeu.pieces;
 
-import fr.amu.iut.earthquakeapp.jeu.Board;
 import fr.amu.iut.earthquakeapp.jeu.Piece;
 import javafx.scene.image.ImageView;
 
@@ -8,16 +7,14 @@ import java.util.ArrayList;
 
 public class Roi extends Piece {
 
-    private static int dernierId =0;
+    private static int dernierId = 0;
+
     public Roi(boolean isWhite, int x, int y) {
         super(isWhite, x, y);
         if (isWhite) {
             super.setImage(new ImageView("/img/image echec/roi_blanc.png"));
             super.setNom("Roi" + dernierId + "blanc");
-
-        }
-
-        else {
+        } else {
             super.setImage(new ImageView("/img/image echec/roi_noir.png"));
             super.setNom("Roi" + dernierId + "noir");
         }
@@ -25,26 +22,29 @@ public class Roi extends Piece {
         this.getImage().setFitHeight(75);
         this.getImage().setFitWidth(75);
         ++dernierId;
-
     }
 
-    //@Override
-    //public void move(int x, int y) {
-
-        //this.setCoordonne(x,y);
-    //}
+    @Override
+    public void move(int x, int y) {
+        setCoordonne(x, y);
+    }
 
     @Override
     public boolean isValide(int x, int y, ArrayList<ArrayList<Piece>> plateau) {
+        int deltaX = Math.abs(x - getX());
+        int deltaY = Math.abs(y - getY());
 
-        return false;
+        // Vérifier si le mouvement est d'une case dans n'importe quelle direction
+        if (deltaX > 1 || deltaY > 1) {
+            return false;
+        }
+
+        // Vérifier si la case d'arrivée est occupée par une pièce de la même couleur
+        Piece destinationPiece = plateau.get(x).get(y);
+        if (destinationPiece != null && destinationPiece.isWhite() == this.isWhite()) {
+            return false;
+        }
+
+        return true;
     }
-    @Override
-    public void move(int x, int y) {
-
-    }
-
-
-
-
 }
